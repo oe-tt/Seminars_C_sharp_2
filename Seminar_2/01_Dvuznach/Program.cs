83debe343d1e2bfccbc98e6403d424644c380987
oe-tt <oe-tt@mail.ru>
2022-12-19 21:58:09 +0300

    Dvuznachnoe

---
+++ Seminar_2/01_Dvuznach/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Clear();
+int number = new Random().Next(10,100);
+Console.WriteLine(number);
+if (number / 10 > number % 10)
+{
+    Console.WriteLine (number / 10);
+}
+else 
+{
+    Console.WriteLine (number % 10);
+}
