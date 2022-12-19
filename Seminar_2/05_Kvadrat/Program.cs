696fe044c50cd7a9ccc08732f06b499b5212a303
oe-tt <oe-tt@mail.ru>
2022-12-19 22:15:32 +0300

    Kvadrat

---
+++ Seminar_2/05_Kvadrat/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.Clear();
+Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a*a == b || b*b == a)
+{
+    Console.WriteLine ("yes");
+}
+else
+{
+    Console.WriteLine ("no");
+}
