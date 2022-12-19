0922a444c005c6fc786b6ff53b6245bf34c45c13
oe-tt <oe-tt@mail.ru>
2022-12-19 22:13:04 +0300

    Trehznachnoe

---
+++ Seminar_2/02_Trehznach/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+Console.Clear();
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+Console.WriteLine(number / 100 * 10 + number % 10);
