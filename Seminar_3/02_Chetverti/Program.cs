8bf61f5e9823553c3722fdbe187fa40be67be3fe
oe-tt <oe-tt@mail.ru>
2022-12-22 21:38:47 +0300

    02_Chetverti

---
+++ Seminar_3/02_Chetverti/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+void quarter(int n)
+{
+    if (n == 1)
+    {
+        Console.WriteLine("x > 0; y > 0");
+    }
+    else if (n == 2)
+    {
+        Console.WriteLine("x < 0; y > 0");
+    }
+    else if (n == 3)
+    {
+        Console.WriteLine("x < 0; y < 0");
+    }
+    else if (n == 4)
+    {
+        Console.WriteLine("x > 0; y < 0");
+    }
+    else
+    {
+        Console.WriteLine("Некорректный номер четверти");
+    }
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите номер четверти");
+int num = int.Parse(Console.ReadLine());
+quarter(num);
