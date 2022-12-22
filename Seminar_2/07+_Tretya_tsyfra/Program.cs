238d8cad012651cfde9575dd3711bd4d6f979bd6
oe-tt <oe-tt@mail.ru>
2022-12-22 19:55:52 +0300

    07+_Tretya_tsyfra

---
+++ Seminar_2/07+_Tretya_tsyfra/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a < 0)
+{
+    a = a * -1;
+}
+if (a < 100)
+{
+    Console.WriteLine("Тетьей цифры нет");
+}
+else
+{
+    if (a < 1000)
+    {
+        Console.Write("Третья цифра числа: ");
+        Console.Write(a % 10);
+    }
+    else
+    {
+        int b = a;
+        int c = a;
+        int i = 1;
+        while (b / 10 > 0)
+        {
+            b = b / 10;
+            i++;
+        }
+        Console.WriteLine("Количество цифр в числе: " + i);
+        while (i > 2)
+        {
+            c = a % 10;
+            a = a / 10;
+            i--;
+        }
+        Console.Write("Третья цифра числа: " + c);
+    }
+}
