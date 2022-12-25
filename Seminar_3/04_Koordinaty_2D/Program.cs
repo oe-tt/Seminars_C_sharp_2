82120b905fc443752ac9217b4defa5574879b3ea
oe-tt <oe-tt@mail.ru>
2022-12-25 22:38:20 +0300

    06_Koordinaty_3D

--- Seminar_3/04_Koordinaty_2D/Program.cs
+++ Seminar_3/04_Koordinaty_2D/Program.cs
@@ -14,4 +14,4 @@ int y2 = int.Parse(Console.ReadLine());
 // Console.WriteLine(Math.Pow((y2-y1),2));
 // Console.WriteLine(Math.Pow((x2-x1),2)-Math.Pow((y2-y1),2));
 Console.Write("Расстояние между точками:");
-Console.WriteLine(Math.Sqrt(Math.Abs(Math.Pow((x2-x1),2)-Math.Pow((y2-y1),2))));
+Console.WriteLine(Math.Sqrt(Math.Abs(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2))));
---
+++ Seminar_3/06_Koordinaty_3D/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.WriteLine("Введите координаты первой точки:");
+Console.Write("x = ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("y = ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("z = ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки:");
+Console.Write("x = ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("y = ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("z = ");
+int z2 = int.Parse(Console.ReadLine());
+Console.Write("Расстояние между точками: ");
+Console.WriteLine(Math.Sqrt(Math.Abs(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2))));
