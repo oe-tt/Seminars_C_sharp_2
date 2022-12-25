6aa57cd76c3f4770b38e25dcf8d90b79bfa7fa99
oe-tt <oe-tt@mail.ru>
2022-12-25 22:13:27 +0300

    04_Koordinaty_2D

---
+++ Seminar_3/04_Koordinaty_2D/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними на плоскости.
+Console.Clear();
+Console.WriteLine("Введите координаты первой точки:");
+Console.Write("x = ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("y = ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки:");
+Console.Write("x = ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("y = ");
+int y2 = int.Parse(Console.ReadLine());
+// Console.WriteLine(Math.Pow((x2-x1),2));
+// Console.WriteLine(Math.Pow((y2-y1),2));
+// Console.WriteLine(Math.Pow((x2-x1),2)-Math.Pow((y2-y1),2));
+Console.Write("Расстояние между точками:");
+Console.WriteLine(Math.Sqrt(Math.Abs(Math.Pow((x2-x1),2)-Math.Pow((y2-y1),2))));
