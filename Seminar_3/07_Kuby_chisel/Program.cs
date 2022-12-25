d79da2be4ce1183fecdec9e3abd160110a92bdf9
oe-tt <oe-tt@mail.ru>
2022-12-25 22:52:49 +0300

    07_Kuby_chisel

---
+++ Seminar_3/07_Kuby_chisel/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+if (N > 0)
+{
+    while (i <= N)
+    {
+        Console.Write(i * i * i + " ");
+        i++;
+    }
+}
+else
+{
+    while (i >= N)
+    {
+        Console.Write(i * i * i + " ");
+        i=i-1;
+    }
+}
