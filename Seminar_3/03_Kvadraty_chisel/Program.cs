6e08551bbfeb20d4500732889b0d72065422f9df
oe-tt <oe-tt@mail.ru>
2022-12-25 21:37:38 +0300

    03_Kvadraty_chisel

---
+++ Seminar_3/03_Kvadraty_chisel/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    Console.Write (i*i + " ");
+    i++;
+}
