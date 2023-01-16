cba025237d63fa46d263275796fa84a426c3f927
oe-tt <oe-tt@mail.ru>
2023-01-16 19:49:48 +0300

    Сумма цифр в числе: добавлена функция

--- Seminar_4/005_Summa_tsifr/Program.cs
+++ Seminar_4/005_Summa_tsifr/Program.cs
@@ -1,14 +1,34 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-Console.Clear();
+/* Console.Clear();
 Console.WriteLine("Введите число");
 int a = int.Parse(Console.ReadLine());
 int b = a;
-if (b < 0)  {b *= -1;}
+if (b < 0) { b *= -1; }
 int sum = 0;
 while (b > 0)
 {
     sum += b % 10;
     b = b / 10;
 }
-Console.WriteLine($"Сумма цифр числа {a} = {sum} ");
+Console.WriteLine($"Сумма цифр числа {a} = {sum} "); */
 
+
+// Через функцию
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int Summa(int a)
+{
+    int b = a;
+    if (b < 0) { b *= -1; }
+    int sum = 0;
+    while (b > 0)
+    {
+        sum += b % 10;
+        b = b / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр числа {a} = {Summa(a)} ");
