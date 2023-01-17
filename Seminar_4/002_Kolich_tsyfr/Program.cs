1f95f4c10a46dc82fbbc885da21b7e5320b5631e
oe-tt <oe-tt@mail.ru>
2023-01-17 17:50:10 +0300

    количество цифр

---
+++ Seminar_4/002_Kolich_tsyfr/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Clear();
+void Colich(int a)
+{
+    int i = 0;
+    if (a == 0) {i = 1;}
+    if (a < 0)  {a *= -1;}
+    int b = a;
+    while (b > 0)
+    {
+        b = b / 10;
+        i++;
+    }
+    Console.WriteLine($"Количество цифр в числе: {i}");
+}
+Console.WriteLine("Введите число");
+Colich(int.Parse(Console.ReadLine()));
