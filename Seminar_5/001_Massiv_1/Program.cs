30da9cef5d7df2610f61962e7ef3a3dbb3fe6565
oe-tt <oe-tt@mail.ru>
2023-01-18 17:49:04 +0300

    Массив случайных чисел с суммой отрицательных и положительных

---
+++ Seminar_5/001_Massiv_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+Console.Clear();
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int [] Array1 = CreateRandomArray(12, -9, 9);
+ShowArray(Array1);
+
+int polojit = 0;
+int otrits = 0;
+
+for (int i = 0; i < Array1.Length; i++)
+{
+    if (Array1[i]>0)
+    {
+        polojit += Array1[i];
+    }
+    else{
+        otrits += Array1[i];
+    }
+}
+
+Console.WriteLine($"Сумма положительных чисел массива = {polojit}\nСумма отрицательных чисел массива = {otrits}");
+
