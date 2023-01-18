31f067ee3fd0128061fd413d2745548eff7cd14d
oe-tt <oe-tt@mail.ru>
2023-01-18 18:29:42 +0300

    массив с заменой знака чисел на противоположный

---
+++ Seminar_5/002_Massiv_zamena_el-tov/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+Console.Clear();
+int[] CreateRandomArray(int N, int start, int end)  // создаем массив случайных чисел
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)     // выводим массив на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] SmenaZnaka(int[] array)   // заменяем знак числа на противоположный
+{
+    for (int i = 0; i < array.Length; i++) array[i] *= -1;
+    return array;
+}
+
+int[] Arr = CreateRandomArray(10, -100, 100);
+ShowArray(Arr);
+Arr = SmenaZnaka(Arr);
+ShowArray(Arr);
