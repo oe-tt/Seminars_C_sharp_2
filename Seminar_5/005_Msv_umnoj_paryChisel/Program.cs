05670e37b8a592ac030b3ef09b3ffcf230b42854
oe-tt <oe-tt@mail.ru>
2023-01-19 21:06:03 +0300

    Произведение пар чисел массива

---
+++ Seminar_5/005_Msv_umnoj_paryChisel/Program.cs
@@ -0,0 +1,40 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+Console.Clear();
+
+int[] RandomArray(int N, int start, int end)           // метод для создания массива случайных чисел в заданном диапазоне
+{
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)                          // метод для вывода массива на экран
+{
+    int N = array.Length;
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int [] ProizvParArray(int [] array)
+{
+    int N = array.Length;
+    int [] ParArray = new int [N/2];
+    for (int i = 0; i < N/2; i++)
+    {
+        ParArray[i] = array[i]*array[N-1-i];
+    }   
+    return ParArray; 
+}
+
+
+
+int[] Array = RandomArray(10, 1, 10);
+PrintArray(Array);
+int[] Pary = ProizvParArray(Array);
+PrintArray(Pary);
