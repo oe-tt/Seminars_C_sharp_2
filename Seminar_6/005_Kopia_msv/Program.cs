f702c036dd50bd012c5699fc5887ff687f8cb9f5
oe-tt <oe-tt@mail.ru>
2023-01-23 08:08:08 +0300

    Создание копии массива

---
+++ Seminar_6/005_Kopia_msv/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+Console.Clear();
+
+int[] RandomArray(int N, int start, int end)           // метод для создания массива случайных чисел в заданном диапазоне
+{
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+int[] CopyArray(int[] origin)                          // метод для создания копии массива
+{
+    int n = origin.Length;
+    int[] copy = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        copy [i] = origin [i];
+    }
+    return copy;
+}
+
+void PrintArray(int[] array)                          // метод для вывода массива на экран
+{
+    int N = array.Length;
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] Array = RandomArray(10, 1, 10);
+Console.WriteLine("Оригинальный массив:");
+PrintArray(Array);
+Console.WriteLine("Копия массива:");
+PrintArray(CopyArray(Array));
