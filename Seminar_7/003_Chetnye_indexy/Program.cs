c359a8a0745834f8cbbc4c76c3aac1f0ef4d66c0
oe-tt <oe-tt@mail.ru>
2023-01-31 21:27:55 +0300

    Замена четных элементов на их квадраты

---
+++ Seminar_7/003_Chetnye_indexy/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)            // метод для создания массива целых чисел
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)                                        // метод для вывода массива на экран
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ZamenaArray(int[,] array)                                           // метод для замены четных элементов массива на их квадраты
+{
+    int m = array.GetLength(0);
+    int n = array.GetLength(1);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j+=2)
+        {
+           array[i, j] *= array[i, j]; 
+        }
+    }
+    return array;
+}
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] Array = GetArray(m, n, 1, 10);
+PrintArray(Array);
+Console.WriteLine();
+ZamenaArray(Array);
+PrintArray(Array);
