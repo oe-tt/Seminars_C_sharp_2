3e2ac80ce70b31e2fab0db09fbbcc4842d9b518c
oe-tt <oe-tt@mail.ru>
2023-01-31 21:17:46 +0300

    двумерный массив по формуле m+n

---
+++ Seminar_7/002_Dv_msv_po_formule/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+    
+Console.Clear();
+
+int[,] GetArray()
+{
+Console.Write("Количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,]array = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)                                        // метод для вывода массива на экран
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Array = GetArray();
+PrintArray(Array);
