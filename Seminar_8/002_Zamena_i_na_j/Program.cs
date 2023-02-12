18d1349814da2aec6e505c06696754a4755881f1
oe-tt <oe-tt@mail.ru>
2023-02-12 23:20:04 +0300

    Транспонирование массива

---
+++ Seminar_8/002_Zamena_i_na_j/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)            // метод для создания массива целых чисел
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)                                        // метод для вывода массива на экран
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Zamena(int[,] array)                                       // метод для транспонирования массива
+{
+    int rows = array.GetLength(1);
+    int colomns = array.GetLength(0);
+    int[,] arrayTransp = new int[rows, colomns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colomns; j++)
+        {
+            arrayTransp[i, j] = array[j, i];
+        }
+    }
+    return arrayTransp;
+}
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] Array = GetArray(m, n, 1, 100);
+PrintArray(Array);
+Console.WriteLine();
+Array = Zamena(Array);
+PrintArray(Array);
+Console.WriteLine();
