15803991f43ea22354519c6ed29e94dba250e751
oe-tt <oe-tt@mail.ru>
2023-02-20 18:51:11 +0300

    Числа от N до 1

---
+++ Seminar_9/005_Rekursia_do_chisla/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// 
+Console.Clear();
+
+void PrintNumbers(int start, int end)
+{
+    Console.Write($"{start} ");
+    if (start == end) return;
+    PrintNumbers(start -1, end);
+}
+
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+while (N < 0)
+{
+    Console.WriteLine("Число N должно быть натуральным.");
+    Console.Write("Введите N: ");
+    N = int.Parse(Console.ReadLine());
+}
+PrintNumbers(N, 1);
