904957ff24567e9f450794e658ae1303b7fa8c54
oe-tt <oe-tt@mail.ru>
2023-02-20 18:53:09 +0300

    Сумма чисел от N до M

---
+++ Seminar_9/006_Rekurs_summa_v_diapazone/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+int SumRecurs(int m, int n) 
+{
+    if (m == n) return m;
+    else return m + SumRecurs(m + 1, n);
+}
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+while (m > n)
+{
+    Console.WriteLine("Число m должно быть меньше n.");
+    Console.Write("Введите m: ");
+    m = int.Parse(Console.ReadLine());
+    Console.Write("Введите n: ");
+    n = int.Parse(Console.ReadLine());
+}
+Console.Write($"Сумма чисел от {m} до {n} = ");
+Console.WriteLine(SumRecurs(m, n));
