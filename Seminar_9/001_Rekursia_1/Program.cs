3a36cda52d8d08022882ee120d697d840cd4adc8
oe-tt <oe-tt@mail.ru>
2023-02-20 20:12:24 +0300

    Числа от 1 до N

--- Seminar_9/001_Rekursia_1/Program.cs
+++ Seminar_9/001_Rekursia_1/Program.cs
@@ -1,16 +1,14 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
-
-Console.WriteLine("Введи значение N");
-
+Console.Clear();
 void PrintNumbers(int start, int end)
 {
     Console.Write($"{start} ");
     if (start == end) return;
     PrintNumbers(start + 1, end);
 }
-
+Console.Write("Введите значение N: ");
 int N = int.Parse(Console.ReadLine());
 while (N < 0)
 {
