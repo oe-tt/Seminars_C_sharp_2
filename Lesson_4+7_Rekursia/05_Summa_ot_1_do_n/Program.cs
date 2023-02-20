43b5573b8442e3d9ffa485b57789ac3e00f57c07
oe-tt <oe-tt@mail.ru>
2023-02-20 18:10:26 +0300

    Сумма чисел от 1 до n

---
+++ Lesson_4+7_Rekursia/05_Summa_ot_1_do_n/Program.cs
@@ -0,0 +1,17 @@
+﻿// Сумма чисел от 1 до n
+Console.Clear();
+int SumFor (int n)          // функция без рекурсии
+{
+    int result = 0;
+    for (int i = 1; i<= n; i++) result +=1;
+    return result;
+}
+
+int SumRecurs (int n)        // с рекурсией
+{
+    if (n == 0) return 0;
+    else return n + SumRecurs (n - 1);
+}
+
+Console.WriteLine (SumFor(10));
+Console.WriteLine (SumRecurs(20));
